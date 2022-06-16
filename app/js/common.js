cd5b34f5e4cc9dc4c517e7ed19291d34ac14f1cf
Ozodbek <o.jumaev@safenetpay.com>
2022-06-17 01:03:37 +0500

    add more section

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,9 @@
-var splide = new Splide( '.splide', {
-    
-} );
-splide.mount();
+(function (){
+
+    let elems = document.getElementsByClassName('splide');
+
+    for(let i = 0; i < elems.length; i++) {
+        new Splide(elems[i]).mount();
+    }
+
+})();
